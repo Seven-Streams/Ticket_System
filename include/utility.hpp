10d97ad744d05c60d0b982fc165fdf8c59433348
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-04-30 10:43:15 +0800

    feat:add the class time.

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,5 +1,17 @@
 #include<iostream>
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
+class time {
+private:
+  int month = 0;
+  int day = 0;
+  int hour = 0;
+  int minute = 0;
+public:
+  ~time() = default;
+  time() = default;
+  time(int, int, int, int);
+  void Add(int);
+};
 std::string ProcessTxt(std::string &);
 #endif
--- src/utility.cpp
+++ src/utility.cpp
@@ -8,13 +8,38 @@ string ProcessTxt(string &txt) {
   while (txt[txt.size() - 1] == ' ') {
     txt.erase(txt.size() - 1, 1);
   }
-  string tmp="";
+  string tmp = "";
   while ((!txt.empty()) && txt[0] != ' ') {
     tmp += txt[0];
     txt.erase(0, 1);
   }
-  while((!txt.empty()) && txt[0] == ' ') {
+  while ((!txt.empty()) && txt[0] == ' ') {
     txt.erase(0, 1);
   }
   return tmp;
+}
+time::time(int a, int b, int c, int d) {
+  month = a;
+  day = b;
+  hour = c;
+  minute = d;
+}
+void time::Add(int duration_minutes) {
+  minute += duration_minutes;
+  int add_hour = minute / 60;
+  minute %= 60;
+  hour += add_hour;
+  int add_day = hour / 24;
+  hour %= 24;
+  day += add_day;
+  if ((month == 6) && (day > 30)) {
+    month++;
+    day -= 30;
+    return;
+  }
+  if (day > 31) {
+    month++;
+    day -= 31;
+    return;
+  }
 }
