fd29a51539f167c212b99facf876a3c011922bde
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-05-18 16:05:59 +0800

    change the size of cache.

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@ class Account;
 extern sjtu::MemoryRiver<Account, 1> account_content;
 extern sjtu::map<HashOfAccount, int, sjtu::Less<HashOfAccount>> account_logged;
 extern sjtu::BPT<int> account_index;
-extern sjtu::BPT<OrderByUser, 19, 20> order_user;
+extern sjtu::BPT<OrderByUser, 19, 20, 200> order_user;
 extern sjtu::BPT<OrderByTrain, 62, 12> queue_list;
 extern sjtu::BPT<int> train_index;
 extern sjtu::MemoryRiver<TrainInfo, 1> train_info;
 extern sjtu::BPT<int> station_database;
-extern sjtu::BPT<TrainDayIndex, 126, 4, 1800> trains_day_index;
+extern sjtu::BPT<TrainDayIndex, 126, 4, 1700> trains_day_index;
 extern sjtu::MemoryRiver<TrainDay, 1> train_day_info;
 void Clean() {
   account_content.clear();
